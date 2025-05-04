ed723d3d3e44a1c091701f752703cb43a54f3993
KiryukhaMe <free42dom@yandex.ru>
2025-05-04 16:35:30 +0500

    Create User Model Manager (PublishedManager of Women)

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
+    'django_extensions', # python manage.py shell_plus --print-sql
     'women.apps.WomenConfig',
 ]
 
